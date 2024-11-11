ccb48bb7efbd95772f7ad1c93d9ccbcccd8817e1
Anubhav-Ghosh1 <iamanubhav11@gmail.com>
2024-11-11 23:37:24 +0530

    Added Controller

--- src/controllers/qr.code.controller.js
+++ src/controllers/qr.code.controller.js
@@ -1,64 +1,65 @@
 import mongoose from "mongoose";
-import {User} from "../models/user.model.js";
+import { User } from "../models/user.model.js";
 import { QR } from "../models/qr.model.js";
 import { v4 as uuidv4 } from 'uuid';
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 
-const generateQRCode = asyncHandler(async (req,res) => {
-    try
-    {
-        const id = req?.user?._id;
-        const {validTill} = req.body;
-        if(!id)
-        {
-            return res.status(400).json(new ApiResponse(400,{},"All fields are required"));
-        }
+// Generate QR code
+const generateQRCode = asyncHandler(async (req, res) => {
+    const userId = req?.user?._id;
+    const { validTill } = req.body;
 
-        const user = await User.findById(id);
-        if(!user)
-        {
-            return res.status(400).json(new ApiResponse(400,{},"User does not exist"));
-        }
-
-        const qrId = uuidv4();
-        const qrDetails = {
-            id,
-            qrId,
-            validTill,
-        }
-
-        const qrCode = await QR.create(qrDetails);
-
-        return res.status(200).json(new ApiResponse(200,qrCode,"QR Code Created Successfully"));
+    if (!userId || !validTill) {
+        return res.status(400).json(new ApiResponse(400, {}, "All fields are required"));
     }
-    catch(e)
-    {
-        return res.status(500).json(new ApiResponse(500,{},"Error while creating QR code"));
+
+    const user = await User.findById(userId);
+    if (!user) {
+        return res.status(404).json(new ApiResponse(404, {}, "User does not exist"));
     }
+
+    const qrId = uuidv4();
+    const qrCode = await QR.create({ userId, qrId, validTill });
+
+    return res.status(201).json(new ApiResponse(201, qrCode, "QR Code Created Successfully"));
 });
 
-const getQRCodeDetails = asyncHandler(async (req,res) => {
-    try
-    {
-        const {qrId} = req.body;
-        if(!qrId)
-        {
-            return res.status(400).json(new ApiResponse(400,{},"All fields are required"));
-        }
+// Fetch QR code details
+const getQRCodeDetails = asyncHandler(async (req, res) => {
+    const { qrId } = req.body;
 
-        const qrDetails = await QR.findById(new mongoose.Types.ObjectId(qrId));
-        if(!qrDetails)
-        {
-            return res.status(500).json(new ApiResponse(500,{},"Error while fetching qr details"));
-        }
-
-        return res.status(200).json(new ApiResponse(200,qrDetails,"QR details fetched successfully"));
+    if (!qrId) {
+        return res.status(400).json(new ApiResponse(400, {}, "QR ID is required"));
     }
-    catch(e)
-    {
-        return res.status(500).json(new ApiResponse(500,{},"Error while fetching QR details"));
-    }
-})
 
-export {generateQRCode, getQRCodeDetails};
+    const qrDetails = await QR.findById(mongoose.Types.ObjectId(qrId));
+    if (!qrDetails) {
+        return res.status(404).json(new ApiResponse(404, {}, "QR code not found"));
+    }
+
+    return res.status(200).json(new ApiResponse(200, qrDetails, "QR details fetched successfully"));
+});
+
+// Validate QR code
+const validateQRCode = asyncHandler(async (req, res) => {
+    const { qrId } = req.params;
+
+    if (!qrId) {
+        return res.status(400).json(new ApiResponse(400, {}, "QR ID is required"));
+    }
+
+    const qrDetails = await QR.findOne({ qrId });
+    if (!qrDetails) {
+        return res.status(404).json(new ApiResponse(404, {}, "QR code not found"));
+    }
+
+    const isValid = new Date(qrDetails.validTill) > new Date();
+    if (!isValid) {
+        return res.status(410).json(new ApiResponse(410, {}, "QR code has expired"));
+    }
+
+    return res.status(200).json(new ApiResponse(200, qrDetails, "QR code is valid"));
+});
+
+export { generateQRCode, getQRCodeDetails, validateQRCode };
